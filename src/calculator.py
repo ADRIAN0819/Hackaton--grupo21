ae33ea104244219e2504ab423aeeea7126c45509
celestesevillano <adriana.sevillano@utec.edu.pe>
2024-08-31 16:56:51 -0500

    agregar-division-celeste

---
+++ src/calculator.py
@@ -0,0 +1,30 @@
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: División por cero"
+
+def calculadora():
+    while True:
+        operacion = input("Escribe tu operación: ")
+        if operacion.lower() == 'c':
+            print("Operación borrada.")
+            continue
+
+        try:
+            resultado = eval(operacion)
+            print(f"Resultado: {resultado}")
+        except Exception as e:
+            print(f"Error: {e}")
+
+if __name__ == "__main__":
+    calculadora()
